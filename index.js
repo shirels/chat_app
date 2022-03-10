2602fa598b262c4efa469d64b5cb518ecdb52e3f
shirels <88611078+shirels@users.noreply.github.com>
2022-03-11 00:45:10 +0200

    chat app - adding more specification for the messages, and documentation

--- index.js
+++ index.js
@@ -6,36 +6,39 @@ const { users } = require('./db/users')
 //initiating server instance
 const app = express()
 
-//using midlware for using the request body
+//using midlware for using the request body - including message parameter(JSON)
 app.use(express.json())
-//midleware for logging the activity
+
+//midleware for monitoring the server traffic
 app.use(async (req,res, next)=>{
-    await fs.appendFile('monitor.log', `${new Date().toISOString()} || ${req.method} - ${req.url}\n`)
+    if(req.method == 'GET'){
+        const {recipient} = req.body
+        await fs.appendFile('monitor.log', `${new Date().toISOString()} || ${req.method} - ${recipient}\n`)
+    }else{
+        const {recipient, sender} = req.body
+        await fs.appendFile('monitor.log', `${new Date().toISOString()} || ${req.method} - ${sender} => ${recipient}\n`)
+    }
     next()
 })
 
-
-app.get('/', (req,res)=>{
+//rout for GET request 
+//retrieving the messages for the recipient inclued in the request body
+app.get('/api/', (req,res)=>{
     const {recipient} = req.body
-    // console.log(recipient)
     const user = users.find(u=>u.username==recipient)
-    // console.log(user)
-    // console.log(users)
     if(user){
         res.status(200).send({msgs:user.messages})
     }
     else{
         res.status(400).send({err: "Sorry, your details are not in the system"})
-    }
-    
+    }   
 })
 
-app.post('/', (req,res)=>{
-    const sender = req.body.sender
-    //console.log(sender)
-    const recipient = req.body.recipient
-    //console.log(recipient)
-    try{
+//rout for POST request with data inclueded in the request body
+//sending a message to another person
+app.post('/api/', (req,res)=>{
+    const {sender, recipient, message} = req.body
+    try{//if sender not in db
         if(!users.some(u=>u.username == sender)){
             users.push(
                 {
@@ -43,25 +46,39 @@ app.post('/', (req,res)=>{
                     messages: []
                 }
             )
-            //console.log(users)
         }
-        if(!users.some(u=>u.username == recipient)){
+        if(!users.some(u=>u.username == recipient)){//if recipient not in db
             users.push(
                 {
                     username: recipient,
-                    messages:[req.body.message]
+                    messages:[
+                        {
+                            sender: sender,
+                            msg: [message]
+                        }
+                    ]
+
                 }
             )
-            //console.log(users)
         }else{
-            const user = users.find(u=>u.username==recipient)
-            user.messages.push(req.body.message)
-            //console.log(user)
+            const rec_user = users.find(u=>u.username==recipient)
+            const rec_msgs = rec_user.messages
+            //if sender hasn't send a message to this recipient yet
+            if(rec_msgs.some(m=>m.sender == sender)){
+                const s_msg = rec_msgs.find(m=>m.sender == sender)
+                s_msg.msg.push(message)
+            }else{
+                rec_msgs.push(
+                    {
+                        sender,
+                        msg: [message]
+                    }
+                )
+            }
         }
-        //console.log(users)
-        res.status(200).send({msg:`The message: "${req.body.message}" was sent to ${recipient}`}) 
+        res.status(200).send({msg:`The message: "${message}" was sent to ${recipient}`}) 
     }catch(err){
-        res.status(400).send({err: err})
+        res.status(400).send({err: err.message})
     }
 })
 
